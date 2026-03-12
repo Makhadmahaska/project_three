4670cf6c0ffb41de0359c461636bbd12aaf04e28
Makhadmahaska <mahadmahaska@hotmail.com>
2026-03-12 16:47:55 +0100

    implemented firebase in the middleware

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,20 +1,25 @@
 import { NextFunction, Request, Response } from "express";
-import jwt, { JwtPayload } from "jsonwebtoken";
-import { Role } from "../../generated/prisma/enums"
-import { jwtSecret } from "../config";
+import admin from "./firebase";
+import { prisma } from "./lib/db"
+import { Role } from "../../generated/prisma/enums";
 
-export interface AuthUser extends JwtPayload {
-  id: number;
-  email: string;
-  role: Role;
-  studentId: number | null;
+export interface AuthUser {
+  uid: string;
+  email?: string;
+  role?: Role;
+  studentId?: number | null;
 }
 
 export interface AuthRequest extends Request {
   user?: AuthUser;
 }
 
-export function requireAuth(req: AuthRequest, res: Response, next: NextFunction): Response | void {
+export async function requireAuth(
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+): Promise<Response | void> {
+
   const header = req.headers.authorization;
 
   if (!header || !header.startsWith("Bearer ")) {
@@ -24,23 +29,28 @@ export function requireAuth(req: AuthRequest, res: Response, next: NextFunction)
   const token = header.split(" ")[1];
 
   try {
-    req.user = jwt.verify(token, jwtSecret) as AuthUser;
+    const decoded = await admin.auth().verifyIdToken(token);
+
+    // lookup user in database
+    const user = await prisma.user.findUnique({
+      where: { firebaseUid: decoded.uid },
+      include: { student: true }
+    });
+
+    if (!user) {
+      return res.status(401).json({ message: "User not registered." });
+    }
+
+    req.user = {
+      uid: decoded.uid,
+      email: decoded.email,
+      role: user.role,
+      studentId: user.student?.id ?? null
+    };
+
     next();
+
   } catch {
     return res.status(401).json({ message: "Invalid or expired token." });
   }
-}
-
-export function requireRole(role: Role) {
-  return function roleMiddleware(req: AuthRequest, res: Response, next: NextFunction): Response | void {
-    if (!req.user) {
-      return res.status(401).json({ message: "Authentication required." });
-    }
-
-    if (req.user.role !== role) {
-      return res.status(403).json({ message: "You are not allowed to access this route." });
-    }
-
-    next();
-  };
 }
