adc4c212d2aa5825930e490a8a2a4c7adf5527eb
Makhadmahaska <mahadmahaska@hotmail.com>
2026-03-12 03:23:19 +0100

    changed index cuz of error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,9 +3,3 @@ import express from 'express';
 const app = express()
 
 app.use(express.json())
-
-app.get("/", (req, res) => {
-  res.json({ message: "Grade Management API running" })
-})
-
-module.exports = app
